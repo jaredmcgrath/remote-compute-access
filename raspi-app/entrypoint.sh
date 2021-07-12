fe8655945654cad3d6e7f794669b69c9acfeb474
jaredmcgrath <jared@pacefactory.com>
2021-07-12 19:43:58 -0400

    Simplify entrypoint; modify Environment

--- raspi-app/entrypoint.sh
+++ raspi-app/entrypoint.sh
@@ -1,25 +1,3 @@
 #!/bin/sh
 
-if [[ -z "${RASPI_APP_ROOT_PATH}" ]]; then
-  this_script_relative_path=$0
-  this_script_full_path=$(realpath $this_script_relative_path)
-  this_script_dir_path=$(dirname $this_script_full_path)
-  app_root_path=$this_script_dir_path/raspi-app
-else
-  app_root_path=$RASPI_APP_ROOT_PATH
-fi
-
-if [[ -z "${RASPI_APP_VENV_PATH}" ]]; then
-  this_script_relative_path=$0
-  this_script_full_path=$(realpath $this_script_relative_path)
-  this_script_dir_path=$(dirname $this_script_full_path)
-  venv_root_path=$this_script_dir_path/venv
-else
-  venv_root_path=$RASPI_APP_VENV_PATH
-fi
-
-venv_path=$venv_root_path/bin/activate
-launch_path=$app_root_path/launch.py
-
-source $venv_path
-python3 $launch_path
+$RASPI_APP_VENV_PATH/bin/python $RASPI_APP_LAUNCH_PATH
