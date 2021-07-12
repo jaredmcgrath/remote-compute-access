a22d326869c5ce1baf2c14a7d127bcd3d01fdb56
jaredmcgrath <jared@pacefactory.com>
2021-07-12 19:21:59 -0400

    Modify entrypoint + systemd unit to be compatible with systemd

--- raspi-app/entrypoint.sh
+++ raspi-app/entrypoint.sh
@@ -1,11 +1,25 @@
 #!/bin/sh
 
-this_script_relative_path=$0
-this_script_full_path=$(realpath $this_script_relative_path)
-this_script_dir_path=$(dirname $this_script_full_path)
-app_root_path=$this_script_dir_path/raspi-app
-venv_path=$this_script_dir_path/venv
+if [[ -z "${RASPI_APP_ROOT_PATH}" ]]; then
+  this_script_relative_path=$0
+  this_script_full_path=$(realpath $this_script_relative_path)
+  this_script_dir_path=$(dirname $this_script_full_path)
+  app_root_path=$this_script_dir_path/raspi-app
+else
+  app_root_path=$RASPI_APP_ROOT_PATH
+fi
 
-cd $app_root_path
-source $venv_path/bin/activate
-python3 launch.py
+if [[ -z "${RASPI_APP_VENV_PATH}" ]]; then
+  this_script_relative_path=$0
+  this_script_full_path=$(realpath $this_script_relative_path)
+  this_script_dir_path=$(dirname $this_script_full_path)
+  venv_root_path=$this_script_dir_path/venv
+else
+  venv_root_path=$RASPI_APP_VENV_PATH
+fi
+
+venv_path=$venv_root_path/bin/activate
+launch_path=$app_root_path/launch.py
+
+source $venv_path
+python3 $launch_path
